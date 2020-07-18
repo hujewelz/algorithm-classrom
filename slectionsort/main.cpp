89fe5582997c36c092e145785133c94861ac2522
Jewelz <hujewelz@163.com>
2020-07-18 11:44:35 +0800

    Add selection sort

---
+++ slectionsort/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#include "sort_test_helper.hpp"
+using namespace std;
+
+template <typename T>
+void selectionSort(T *arr, int n) {
+  for (int i = 0; i < n; i++) {
+    // 查找 (i, n)区间内的最小值
+    int minIndex = i;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    swap(arr[i], arr[minIndex]);
+  }
+}
+
+template <typename T>
+void selectionSortC(T *arr, int n, bool (*compare)(T, T)) {
+  for (int i = 0; i < n; i++) {
+    int index = i;
+    for (int j = i + 1; j < n; j++) {
+      if (compare(arr[j], arr[index])) {
+        index = j;
+      }
+    }
+    swap(arr[i], arr[index]);
+  }
+}
+
+template <typename T>
+bool _less(T a, T b) {
+  return a < b;
+}
+
+template <typename T>
+bool larger(T a, T b) {
+  return a > b;
+}
+
+int main(int argc, char *argv[]) {
+  int n = 10000;
+  int *arr = sort::genRandomArray(n, 1, n);
+  selectionSortC(arr, n, _less);
+  sort::printArray(arr, n);
+  cout << endl;
+
+  delete[] arr;
+  return 0;
+}
---
+++ slectionsort/sort_test_helper.hpp
@@ -0,0 +1,29 @@
+#ifndef SORT_TEST_HELPER_H
+#define SORT_TEST_HELPER_H
+
+#include <ctime>
+#include <iostream>
+
+namespace sort {
+
+int *genRandomArray(int n, int rangeL, int rangeR) {
+  assert(rangeL < rangeR);
+  int *arr = new int[n];
+  srand(time(NULL));
+  for (int i = 0; i < n; i++) {
+    arr[i] = rand() % (rangeR - rangeL + 1) + rangeL;
+  }
+  return arr;
+}
+
+template <typename T>
+void printArray(T arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    std::cout << arr[i] << ", ";
+  }
+  std::cout << std::endl;
+}
+
+}  // namespace sort
+
+#endif  // SORT_TEST_HELPER_H
